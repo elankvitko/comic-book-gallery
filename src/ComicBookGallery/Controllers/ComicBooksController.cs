dbb70fb4fc09e722dc5c8dfede35f178f1dfba1f
Elan Kvitko <elan.kvitko@gmail.com>
2019-01-13 12:28:49 -0500

    Add comicbook and artist models

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -14,11 +14,11 @@ namespace ComicBookGallery.Controllers
             ViewBag.IssueNumber = 700;
             ViewBag.Description = "<p>This is a description</p>";
             ViewBag.AuthorProps = new string[] {
-                "Stringy",
-                "Bingy",
-                "Hangry",
-                "Bindy",
-                "Windy"
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
             };
             return View();
         }
